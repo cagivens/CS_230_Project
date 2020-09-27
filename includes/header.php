6593527aa0e6afe8ee1c2558ebd1651808abd8f6
Caleb Givens <33668778+cagivens@users.noreply.github.com>
2020-09-27 14:26:13 -0400

    Completed 'Logout.php'

--- includes/header.php
+++ includes/header.php
@@ -34,7 +34,12 @@ session_start();
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="login.php">Login</a>
+                    <?php
+                    if(isset($_SESSION['uid']))
+                        echo '<a class="nav-link" href="includes/logout.php">Logout</a>';
+                    else
+                        echo '<a class="nav-link" href="login.php">Login</a>';
+                    ?>              
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
