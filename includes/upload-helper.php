a6a0f00ef8f02a188e53d9ab83b0e21a371ca98a
Caleb Givens <33668778+cagivens@users.noreply.github.com>
2020-10-04 21:49:31 -0400

    Update upload-helper.php

--- includes/upload-helper.php
+++ includes/upload-helper.php
@@ -7,5 +7,52 @@ define('MB', 1048576);
 if (isset($_POST['prof-submit'])) {
     require 'dbhandler.php';
    
+    $username = $_SESSION['username'];
 
+    $file = $_FILES['prof-image'];
+    $file_name = $file['name'];
+    $file_tmp_name = $file['tmp_name'];
+    $file_error = $file['error'];
+    $file_size = $file['size'];
+
+    $bio = $_POST['bio'];
+
+    $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+    $allowed = array('jpg', 'jpeg', 'png', 'svg');
+
+    if($file_error !== 0) {
+        header("Location: ../profile.php?error=UploadError");
+        exit();
+    }
+
+    if(!in_array($ext, $allowed)) {
+        header("Location: ../profile.php?error=InvalidType");
+        exit();
+    }
+
+    if($file_size > 4 * MB) {
+        header("Location: ../profile.php?error=FileSizeExceeded");
+        exit();
+    }
+
+    $new_name = uniqid('', true).".".$ext;
+    $destination = '../uploads/'.$new_name;
+
+    $sql = "UPDATE profile SET picpath=?, bio=? WHERE username=?;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: ../profil.php?error=SqlInjectionError");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "sss", $destination, $bio, $username);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+
+    move_uploaded_file($file_tmp_name, $destination);
+
+    header("Location: ../profil.php?upload=Success");
+    exit();
 }
